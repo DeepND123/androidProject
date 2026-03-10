e66f07035e98ae764c811d1b584b90ed0a3d89d3
Nethma Deepachandi <nethma2021lp@gmail.com>
2026-03-11 00:00:59 +0530

    add login page

--- app/src/main/java/com/example/practiceone/LoginPage.java
+++ app/src/main/java/com/example/practiceone/LoginPage.java
@@ -1,4 +1,4 @@
-package com.example.practiceone.firstPage;
+package com.example.practiceone;
 
 import android.os.Bundle;
 
@@ -8,15 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.practiceone.R;
-
-public class firstActivity extends AppCompatActivity {
+public class LoginPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_first);
+        setContentView(R.layout.activity_login_page);
 
     }
 }
