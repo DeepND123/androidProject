c8fa6d9a062de581b04a469319273482273b4c5e
Nethma Deepachandi <nethma2021lp@gmail.com>
2026-03-10 23:48:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/practiceone/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.practiceone;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.practiceone.firstPage.firstActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button=findViewById(R.id.button);
+                button.setOnClickListener(v -> {
+
+                        Intent intent=new Intent(
+                                MainActivity.this,
+                                firstActivity.class);
+                        startActivity(intent);
+
+                });
+    }
+}
