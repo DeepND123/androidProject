3e25e9d516e63c4ea7a243a3418d51c66591eced
Nethma Deepachandi <nethma2021lp@gmail.com>
2026-03-12 21:37:50 +0530

    set login button as a first loanch page

--- app/src/main/java/com/example/practiceone/MainActivity.java
+++ app/src/main/java/com/example/practiceone/MainActivity.java
@@ -9,21 +9,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.practiceone.firstPage.LoginPage;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button=findViewById(R.id.button);
-                button.setOnClickListener(v -> {
 
-                        Intent intent=new Intent(
-                                MainActivity.this,
-                                LoginPage.class);
-                        startActivity(intent);
-
-                });
     }
 }
--- app/src/main/java/com/example/practiceone/firstPage/RegisterPage.java
+++ app/src/main/java/com/example/practiceone/firstPage/RegisterPage.java
@@ -36,6 +36,7 @@ public class RegisterPage extends AppCompatActivity {
             }
         });
 
+
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
