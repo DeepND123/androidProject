22e40f4f0b3ba088bf434fafd1416122c37048c6
Nethma Deepachandi <nethma2021lp@gmail.com>
2026-03-14 16:49:48 +0530

    add a new page

--- app/src/main/java/com/example/practiceone/firstPage/firstActivity.java
+++ app/src/main/java/com/example/practiceone/firstPage/firstActivity.java
@@ -1,6 +1,10 @@
 package com.example.practiceone.firstPage;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,5 +22,17 @@ public class firstActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_first);
 
+        Button btnGo = findViewById(R.id.btnGo);
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(firstActivity.this, Dashboard.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
     }
 }
